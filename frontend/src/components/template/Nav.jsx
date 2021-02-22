268c14b32b9ca81d520e47180dfafd6832ebdeec
michelangelo7 <m_che_l@hotmail.com>
2021-02-21 21:11:33 -0300

    Fix immer vulnerability

---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,15 @@
+import './Nav.css'
+import React from 'react'
+
+export default props =>
+    <aside className="menu-area">
+        <nav className="menu">
+            <a href="#">Home</a>
+            <a href="#">Chat</a>
+            <a herf="#">Feed</a>
+            <a href="#">Contatos</a>
+            <a href="#">Albuns</a>
+            <a href="#">Avalicoes</a>
+            <a href="#">Dados</a>
+        </nav>
+    </aside>
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css'
+
+import Nav from '../components/template/Nav'
+
+export default props => 
+    <div>
+       <Nav />
+    </div>
