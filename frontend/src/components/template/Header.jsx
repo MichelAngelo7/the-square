10d08a3777ba8d3026c93b54ba2a034fc3b58cc7
michelangelo7 <m_che_l@hotmail.com>
2021-02-24 01:55:35 -0300

    Test new layout

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default props =>
     <header className="header">
-        <h1 className="header">
+        <h1 className="mt-3">
             <i className={`fa fa-${props.icon}`}></i>{props.title}
         </h1>
     <p className="lead text-muted">{props.subtitle}</p>
